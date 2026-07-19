c4838bbc51b9ea3ceedbc8ce5a06367c580c76c3
lavankoushik35-art <lavankoushik35@gmail.com>
2026-07-19 21:31:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-// Welcome message
+// ===============================
+// KK FISH CLUB
+// script.js
+// ===============================
+
+// Welcome
 console.log("Welcome to KK FISH CLUB");
 
-// Button animation
-document.querySelectorAll("button").forEach(btn=>{
-btn.addEventListener("click",()=>{
-btn.style.transform="scale(0.95)";
+// Button click animation
+document.querySelectorAll("button").forEach(button=>{
+button.addEventListener("click",()=>{
+button.style.transform="scale(0.95)";
 setTimeout(()=>{
-btn.style.transform="scale(1)";
+button.style.transform="scale(1)";
 },150);
 });
 });
+
+// Confirmation Codes
+const validCodes=[
+"KKFISHGDK",
+"KKFISHHEAVEN",
+"KKAQUA",
+"KKAQUARIUMS",
+"GDKHEAVENFISHES"
+];
+
+// Check Confirmation Code
+function checkCode(){
+
+const code=document.getElementById("code");
+
+if(!code) return;
+
+if(validCodes.includes(code.value.toUpperCase
